aa72816f02028b2bdfaf4193d28c458620d7aefb
Aliba Aslam <alibaaslam33@gmail.com>
2026-06-04 01:39:29 +0530

    Time: 0 ms (100%), Space: 16.8 MB (58.06%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -3,11 +3,14 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n= nums.size();
         vector<int>ans(2*n);
-        for(int i=0; i<=(n-1); i++)
-        {
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
-        }
-        return ans;
+       for(int i=0; i<n;i++)
+       {
+        ans[i]=nums[i];
+       }
+       for(int i=n; i<2*n;i++)
+       {
+        ans[i]=nums[i-n];
+       }
+       return ans;
     }
 };
