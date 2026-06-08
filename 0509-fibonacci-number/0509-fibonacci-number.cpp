14889cce7cb464af7e951298e10fbb116c33f829
Aliba Aslam <alibaaslam33@gmail.com>
2026-06-09 00:33:56 +0530

    Time: 3 ms (63%), Space: 8.3 MB (11.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-         return func(n);
-      
+        if (n <= 1) return n;
+        vector<int> dp(n + 1, 0);  
+        return func(n, dp);
     }
-       int func(int n)
-      {
-         if(n<=1)
-       return n; 
-       return(fib(n-2)+fib(n-1));
-      }
+
+    int func(int n, vector<int>& dp) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];
         
+        dp[n] = func(n - 1, dp) + func(n - 2, dp);
+        return dp[n];
+    }
 };
