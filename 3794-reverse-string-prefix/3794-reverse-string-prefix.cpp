b320623e4f0f6aa9d0037ace74a2bdc9c19edb42
Aliba Aslam <alibaaslam33@gmail.com>
2026-06-24 23:05:40 +0530

    Time: 0 ms (100%), Space: 8.9 MB (21.31%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int i = 0;
+        k = k - 1;
+
+        while (i < k) {
+            char temp = s[i];
+            s[i] = s[k];
+            s[k] = temp;
+
+            i++;
+            k--;
+        }
+
+        return s;
+    }
+};
