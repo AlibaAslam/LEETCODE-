cd0545ce3765b2a2b39ac15e1553ba1094e37644
Aliba Aslam <alibaaslam33@gmail.com>
2026-06-28 15:52:55 +0530

    Time: 0 ms (100%), Space: 13.3 MB (71.73%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head == nullptr) return head;
 
-        vector<int> vec;
-        ListNode* temp = head;
+        //CHEAT CODE APPROACH
+        // if (head == nullptr) return head;
+
+        // vector<int> vec;
+        // ListNode* temp = head;
         
-        while (temp != nullptr) {
-            vec.push_back(temp->val);
-            temp = temp->next;
+        // while (temp != nullptr) {
+        //     vec.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        
+        // int n = vec.size() - 1;
+        // ListNode* helper = head; 
+        // while (helper != nullptr) {
+        //     helper->val = vec[n];
+        //     n--;
+        //     helper = helper->next;
+        // }
+        
+        // return head;
+
+        //OPTIMISED APPROACH
+        ListNode* curr= head;
+        ListNode* prev =nullptr;
+        while(curr !=nullptr)
+        {
+            ListNode*next = curr->next;
+            curr->next= prev;
+            prev = curr;
+            curr =next;
         }
-        
-        int n = vec.size() - 1;
-        ListNode* helper = head; 
-        while (helper != nullptr) {
-            helper->val = vec[n];
-            n--;
-            helper = helper->next;
-        }
-        
-        return head;
+        head=prev;
+    return head;
     }
 };
