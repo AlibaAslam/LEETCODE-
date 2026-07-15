d1fc5e3f691774128b21064d24b3823b9645ac36
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-15 14:36:47 +0530

    Time: 0 ms (100%), Space: 14.3 MB (5.71%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
@@ -6,7 +6,7 @@ public:
      
       for(int i: arr)
       {
-        if(i <= 500)
+        if(i <= 500)// to ensure that i should be under constraint and value does not exceed from 500 , it should be from 0 to 500 only
         {
           freq[i]++;
         }
@@ -23,3 +23,31 @@ public:
       return maxm;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findLucky(vector<int>& arr) {
+//       vector<int>freq(501,0);
+//       int maxm=-1;
+//      
+//       for(int i: arr)
+//       {
+//         freq[i]++;
+//       }
+//       for(int i=0; i<=freq.size()-1; i++)
+//       {
+//         if(freq[i]==i )
+//         {
+//            int ans=freq[i];
+//              maxm=max(maxm,ans);
+//         }
+//        
+//         
+//      }
+//         
+//       
+//       return maxm;
+//         
+//     }
+// };
