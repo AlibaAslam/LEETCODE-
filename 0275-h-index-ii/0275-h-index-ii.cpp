d8dfc823ac593dfb577c177ca3b4e18f57ed8a10
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-18 21:26:59 +0530

    Time: 0 ms (100%), Space: 22.6 MB (46.77%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n= citations.size();
+        int ans=0;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(citations[i]>=ans+1)
+            {
+               ans++;
+            }
+           else{
+            break;
+           }
+        }
+        return ans;
+    }
+};
