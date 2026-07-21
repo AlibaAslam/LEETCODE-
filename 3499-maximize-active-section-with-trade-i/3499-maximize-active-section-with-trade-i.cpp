c859b8f035c25ac8178297620c03a44fbda0921c
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-22 00:28:48 +0530

    Time: 63 ms (62.16%), Space: 79.2 MB (42.34%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,44 +1,34 @@
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    int maxActiveSectionsAfterTrade(std::string s) {
-        // Step 1: Augment string with '1' at both ends
-        std::string t = "1" + s + "1";
-        
-        int total_ones = 0;
-        int max_trade_gain = 0;
-        int prev_zero_len = -1;
-        
-        int n = t.length();
+    int maxActiveSectionsAfterTrade(string s) {
+        string t = "1" + s + "1";
+        int ans = 0;
+        int pre = -1;
+        int maxZero = 0;
         int i = 0;
-        
-        // Two-pointer run-length encoding
-        while (i < n) {
-            int j = i;
-            while (j < n && t[j] == t[i]) {
+
+        while (i < t.length()) {
+            int j = i + 1;
+
+            while (j < t.length() && t[j] == t[i]) {
                 j++;
             }
-            
-            int len = j - i;
-            
+
+            int cur = j - i;
+
             if (t[i] == '1') {
-                total_ones += len;
-            } else {
-                // Check sum with the previous adjacent zero block
-                if (prev_zero_len != -1) {
-                    max_trade_gain = std::max(max_trade_gain, prev_zero_len + len);
+                ans += cur;
+            } 
+            else {
+                if (pre != -1) {
+                    maxZero = max(maxZero, pre + cur);
                 }
-                prev_zero_len = len;
+                pre = cur;
             }
-            
+
             i = j;
         }
-        
-        // Subtract the 2 augmented '1's
-        total_ones -= 2;
-        
-        return total_ones + max_trade_gain;
+
+        return ans - 2 + maxZero;
     }
 };
