0afe6e6de155551bea88dec06d8fd53183daeb23
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-23 00:42:53 +0530

    Time: 32 ms (32%), Space: 177.8 MB (65.07%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -6,12 +6,12 @@ class Solution {
 public:
     int climbStairs(int n, std::vector<int>& costs) {
         std::vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0; // Base case: starting at stair 0 costs nothing
+        dp[0] = 0; 
 
         for (int i = 1; i <= n; i++) {   
             for (int j = 1; j <= 3; j++) {
                 if (i - j >= 0 && dp[i - j] != INT_MAX) {
-                    // Difference squared: (j)^2 or (i - (i - j))^2 is just j * j
+                    
                     int stepCost = costs[i - 1] + j * j; 
                     dp[i] = std::min(dp[i], dp[i - j] + stepCost);
                 }
