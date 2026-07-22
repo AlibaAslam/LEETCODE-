7cf96548109d6ef279a2b6071af519aa16d78454
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-22 23:35:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int stair(int n, vector<int>&dp)
-    {
-        if(n<=1)
-        return 1;
+    // int stair(int n, vector<int>&dp)
+    // {
+    //     if(n<=1)
+    //     return 1;
 
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-      dp[n] = stair(n-1, dp)+stair(n-2,dp); 
-      return dp[n];
-    }
+    //     if(dp[n]!=-1)
+    //     {
+    //         return dp[n];
+    //     }
+    //   dp[n] = stair(n-1, dp)+stair(n-2,dp); 
+    //   return dp[n];
+    // }
     int climbStairs(int n) {
 
         // THIS RECURSION APPROACH GIVES TIME LIMIT EXCEEDED
@@ -19,9 +19,21 @@ public:
         // return 1;
         //  return climbStairs(n-1)+climbStairs(n-2);
 
+
+        // THIS APPRAOCH IS TOP DOWN RECURSION APPROACH
+        // vector<int>dp(n+1,-1);
+        // return stair(n, dp);
+
+
+        // THIS APPROACH IS BOTTOM DP APPROACH
         vector<int>dp(n+1,-1);
-        return stair(n, dp);
-        
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
 
 
         
