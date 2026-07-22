44b11ed219d983b0299f919f68a8390eb11f5596
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-22 23:41:13 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -26,16 +26,28 @@ public:
 
 
         // THIS APPROACH IS BOTTOM DP APPROACH
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+        // vector<int>dp(n+1,-1);
+        // dp[0]=1;
+        // dp[1]=1;
+        // for(int i=2; i<=n; i++)
+        // {
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+
+        // THIS APPROACH IS SPACE OPTIMISED BEST APPROACH
+        if(n<=1)
+        return 1;
+        int prev1=1;// similar to dp[1]
+        int prev2=1;// similar to dp[0]
         for(int i=2; i<=n; i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n];
 
-
+    return prev1;
         
     }
 };
