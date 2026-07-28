dbf97264f5bc16cc3b2244d9c82a57e63b902dde
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-29 00:39:28 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        int j=0;
-        for(i=0; i<nums.size(); i++)
-        {
-            if(nums[i]!=0 && nums[j]==0)
-            {
-              nums[i]=nums[i]-nums[j];
-              nums[j]=nums[i]+nums[j];
-              nums[i]=nums[j]-nums[i];
-                
+        int j = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0 && nums[j] == 0) {
+                nums[i] = nums[i] - nums[j];
+                nums[j] = nums[i] + nums[j];
+                nums[i] = nums[j] - nums[i];
             }
-            if(nums[j]!=0)
-            {
+            if (nums[j] != 0) {
                 j++;
             }
         }
-
-        
     }
 };
