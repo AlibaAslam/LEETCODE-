754a5095014d252c7fe9148f2bad45710cee76db
Aliba Aslam <alibaaslam33@gmail.com>
2026-07-30 23:42:36 +0530

    Time: 0 ms (100%), Space: 15.2 MB (52.06%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -6,6 +6,8 @@ public:
         vector<int>ans;
         for(int i=0; i<=nums.size()-1;i++)
         {
+            if(nums[i]>target)
+            break;
             if(nums[i]==target)
             {
                 ans.push_back(i);
