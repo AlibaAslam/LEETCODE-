cf28fa23cb29e88f46ae7cf31d5ded617ca861ab
Aliba Aslam <alibaaslam33@gmail.com>
2026-08-11 11:00:43 +0530

    Time: 2 ms (9.29%), Space: 24.4 MB (66.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -10,18 +10,36 @@
  * };
  */
 class Solution {
-    void helper(TreeNode* root, vector<int>& ans) {
-        if (root == nullptr)
-            return;
-        helper(root->left, ans);
-        ans.push_back(root->val);
-        helper(root->right, ans);
-    }
+    // void helper(TreeNode* root, vector<int>& ans) {
+    //     if (root == nullptr)
+    //         return;
+    //     helper(root->left, ans);
+    //     ans.push_back(root->val);
+    //     helper(root->right, ans);
+    // }
 
 public:
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        helper(root, ans);
-        return ans[k - 1];
+        // vector<int> ans;
+        // helper(root, ans);
+        // return ans[k - 1];
+        stack<TreeNode*>st;
+        TreeNode*curr=root;
+        while(curr!=nullptr || !st.empty())// go to deepest left of the tree
+        {
+            while(curr!=nullptr)
+            {
+                st.push(curr);
+                curr=curr->left;
+            }
+            curr= st.top();// pop from stack
+            st.pop();
+            if(--k==0)
+            {
+                return curr->val;// early stop
+            }
+            curr=curr->right;// go to right 
+        }
+        return -1;
     }
 };
